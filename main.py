2a6509dc907b5f0dc93c5941be08560d418ca1b1
Robert Barreca <barreca@wisc.edu>
2023-11-02 11:09:16 -0500

    added comments for clarity

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main():
-    print("please enter a word")
+    """
+    main asks the user to input a string, then parses that string and calls the toPigLatin function on every word in the string. 
+    It additionally makes sure all the punctuation is correct based on user input. Finally it then concatenates all the returned
+    words from the toPigLatin() and prints the pig latin translation of the user input
+    """ 
+    print("please enter some english")
     text = input()
     retVal = ""
     for word in text.split(" "):
@@ -17,6 +22,13 @@ def main():
 
 
 def toPigLatin(word):
+    """
+    toPigLatin takes in a word and first checks to see if the first letter is capital, if it is is then passes it to toPigLatinUC().
+    Otherwise it converts the word to pig latin itself
+
+    :param word: the word to be converted to pig latin
+    :return: the pig latin translation of the word argument
+    """
     # first letter is upper case
     if (word[0].isupper()):
         return toPigLatinUC(word)
@@ -39,6 +51,12 @@ def toPigLatin(word):
     
 
 def toPigLatinUC(word):
+    """
+    toPigLatinUC converts a word that begins with an upper case letter to pig latin.
+
+    :param word: the word to be converted to pig latin
+    :return: the pig latin translation of the word argument
+    """
     # word starts with a vowel
     if (word[0] == 'A' or word[0] == 'E' or word[0] == 'I' or word[0] == 'O' or word[0] =='U'):
         return word + "ay"
