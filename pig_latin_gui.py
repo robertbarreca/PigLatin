8668e57b4b5fbdca5d82bd21e144caa8ef910e75
Robert Barreca <barreca@wisc.edu>
2023-11-28 17:25:10 -0600

    design much better

--- pig_latin_gui.py
+++ pig_latin_gui.py
@@ -3,30 +3,40 @@ import main
 
 pygame.init()
 # screen dimensions
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 500
 
 # setup pygame window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("English to Pig Latin Translator")
 
 base_font = pygame.font.Font(None, 18)
+title_font = pygame.font.Font(None, 36)
+header_font = pygame.font.Font(None, 24)
+
+title_text = "English to Pig Latin Translator"
 input_text = ""
 output_text = ""
 active = False
 
 # create input text box
-input_rect = pygame.Rect(200, 200, 200, 32)
+text_box_width = 400
+text_box_x = (SCREEN_WIDTH - text_box_width) // 2
+input_text_box_y = 150
+input_rect = pygame.Rect(text_box_x, input_text_box_y, text_box_width, 32)
 active_color = pygame.Color(255, 255, 255)
 inactive_color = pygame.Color('gray15')
 input_color = inactive_color
 
 # create output text box
-output_rect = pygame.Rect(200, 250, 200, 32)
+output_text_box_y = 250
+output_rect = pygame.Rect(text_box_x, output_text_box_y, text_box_width, 32)
 output_color = inactive_color
 
 # create a translate button
-translate_button = pygame.Rect(200, 300, 140, 50)
+button_width = 140
+button_x = (SCREEN_WIDTH - button_width) // 2
+translate_button = pygame.Rect(button_x, 325, button_width, 50)
 button_color = pygame.Color('pink')
 button_text = "Translate"
 
@@ -83,21 +93,36 @@ while run:
 
     # draw button
     pygame.draw.rect(screen, button_color, translate_button)
-    button_font = pygame.font.Font(None, 18)
-    button_surface = button_font.render(button_text, True, (0, 0, 0))
+    button_surface = base_font.render(button_text, True, (0, 0, 0))
     text_width, text_height = button_surface.get_size()
     screen.blit(button_surface, (translate_button.x + (translate_button.width -
                 text_width) // 2, translate_button.y + (translate_button.height - text_height) // 2))
 
+    # Render Title
+    title_surface = title_font.render(title_text, True, (255, 255, 230))
+    title_width, title_height = title_surface.get_size()
+    screen.blit(title_surface, ((SCREEN_WIDTH - title_width) // 2, 50))
+
+    # Render Headers
+    header1_surface = header_font.render(
+        "Enter some english here:", True, (255, 255, 230))
+    header1_width, header1_height = header1_surface.get_size()
+    screen.blit(header1_surface, (text_box_x + 10, input_text_box_y - 25))
+
+    header2_surface = header_font.render(
+        "Translated text:", True, (255, 255, 230))
+    header2_width, header2_height = header2_surface.get_size()
+    screen.blit(header2_surface, (text_box_x + 10, output_text_box_y - 25))
+
     # Render input text inside input rect
-    text_surface = base_font.render(input_text, True, (255, 255, 255))
-    screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
-    input_rect.w = max(200, text_surface.get_width() + 10)
+    input_surface = base_font.render(input_text, True, (255, 255, 255))
+    screen.blit(input_surface, (input_rect.x + 5, input_rect.y + 5))
+    input_rect.w = max(400, input_surface.get_width() + 10)
 
     # Render output text in output text box
-    text_surface = base_font.render(output_text, True, (255, 255, 255))
-    screen.blit(text_surface, (output_rect.x + 5, output_rect.y + 5))
-    output_rect.w = max(200, text_surface.get_width() + 10)
+    output_surface = base_font.render(output_text, True, (255, 255, 255))
+    screen.blit(output_surface, (output_rect.x + 5, output_rect.y + 5))
+    output_rect.w = max(400, output_surface.get_width() + 10)
 
     pygame.display.flip()
 
